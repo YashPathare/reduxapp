1e4cbf6e5819898a1cb254f8df09c72935b24766
YashPathare <yashpathare51@gmail.com>
2025-03-10 16:41:44 +0530

    76 7:13

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Shop() {
+  return (
+    <div>
+        <h2>Deposit/Withdraw Money</h2>
+        <button className="btn btn-secondary mx-2">-</button>
+        Update Balance
+        <button className="btn btn-success mx-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,11 @@
+export const depositMoney=(amount)=>{
+    return (dispatch)=>{
+        dispatch({
+            type:'deposit',
+        })
+    }
+}
+
+export const withdrawMoney=(amount)=>{
+    
+}
